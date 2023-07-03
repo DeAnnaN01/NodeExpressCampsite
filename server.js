07acb006fa5e2d2543f4fec75553dfdc65e77193
DeAnna Nicholson <13dpn13@gmail.com>
2023-07-02 23:08:32 -0700

    Intro to Express Part 2

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 
 const express = require('express');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res) => {
+    res.statusCode = 200;
+});
 
 
 app.use((req, res) => {

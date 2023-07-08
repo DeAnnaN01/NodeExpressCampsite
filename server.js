1b5e6bb3d022216223a51b3a1f02df9576f8ebed
DeAnna Nicholson <13dpn13@gmail.com>
2023-07-08 09:02:06 -0700

    ready for workshop 1

--- server.js
+++ server.js
@@ -15,24 +15,6 @@ app.use('/campsites', campsiteRouter);
 
 
 
-app.get('/campsites/:campsiteId', (req, res) => {
-    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
-});
-
-app.post('/campsites/:campsiteId', (req, res) => {
-    res.statusCode = 403;
-    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
-});
-
-app.put('/campsites/:campsiteId', (req, res) => {
-    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
-    res.end(`Will update the campsite: ${req.body.name} 
-        with description: ${req.body.description}`);
-});
-
-app.delete('/campsites/:campsiteId', (req, res) => {
-    res.end(`Deleting campsite: ${req.params.campsiteId}`);
-});
 
 
 app.use(express.static(__dirname + '/public'));
